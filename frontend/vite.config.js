16a215965934ac8639ebc56d48e1ba42c38ed5b0
chinmayakumardas2000@gmail.com <chinmayakumardas2000@gmail.com>
2024-12-29 01:12:21 +0530

    meetnotes folder designed

---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Makes your app accessible over your local network
+    port: 3000,      // Custom port if needed
+  },
+});
