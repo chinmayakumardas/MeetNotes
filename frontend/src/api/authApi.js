8c7f242cf046d1063b5c402e76c510866c930861
chinmayakumardas2000@gmail.com <chinmayakumardas2000@gmail.com>
2024-12-30 09:52:56 +0530

    file added

--- frontend/src/api/authApi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:3000"; // Replace with your db.json server URL.
-
-export const login = async (credentials) => {
-  const response = await axios.post(`${API_URL}/login`, credentials);
-  return response.data;
-};
