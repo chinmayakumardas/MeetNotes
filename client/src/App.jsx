a0a1d9d5518cecffc759a613d81feeac7ee82631
chinmayakumardas2000@gmail.com <chinmayakumardas2000@gmail.com>
2025-01-02 08:09:37 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import LoginForm from './components/login/LoginForm';
-function App() {
+import React from 'react'
+import LoginPage from './pages/LoginPage'
+
+const App = () => {
   return (
-    <div className="p-4 bg-gray-100 min-h-screen">
-      <h1 className="text-2xl font-bold">Hello Vite + Tailwind + MUI</h1>
-      <Button variant="contained" color="primary" className="mt-4">
-        Material UI Button
-      </Button>
-      {/* <LoginForm/> */}
+    <div>
+      App
+      <LoginPage/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import LoginForm from '../components/login/LoginForm'
 
 const LoginPage = () => {
   return (
     <div>
       login page
+      <LoginForm/>
     </div>
   )
 }
